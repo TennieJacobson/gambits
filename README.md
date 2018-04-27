ccccb348029fabf4efbcc2679c5c481b3c43f01a
seth-w-gilbert <34290970+seth-w-gilbert@users.noreply.github.com>
2018-04-27 17:39:57 -0500

    Add files via upload
    upload files from day 1 with Tennie

---
+++ README.md
@@ -0,0 +1,72 @@
+
+# GamBits
+By Seth Gilbert and Tennie Jacobson
+
+## Purpose
+To make it easier to understand and do bit manipulation for data compression and cryptography.
+
+## Description/Features
+GamBits has understandable bit wise operators and a built in BitString type. Our goal was to make it readable, intuitive, and not overly verbose. There are built in dictionaries to make hash tables easy. Trees can be made with a combination of dictionaries and arrays. With these tools, the developer will be able to do data compression and cryptography without the headache. Our powerful interpreter is written in JavaScript.
+
+## Grammar
+
+```
+program
+  :  statement* EOF
+
+statement
+  :  IDENTIFIER ASSIGNMENT expression
+  |  IF LEFT_P expression RIGHT_P THEN statement* (ELSE statement*)? DONE
+  |  DEFINE IDENTIFIER LEFT_P (IDENTIFIER COMMA)* IDENTIFIER RIGHT_P statement* DONE
+  |  WHILE LEFT_P expression RIGHT_P statement* DONE
+  |  PRINT expression
+  |  (IDENTIFIER DOT)? IDENTIFIER LEFT_P (expression COMMA)* expression RIGHT_P
+  |  MULTI_COMMENT .* MULTI_COMMENT
+  |  OCTOTHORP .* \n
+
+expression
+  :  BITSTRING
+  |  INTEGER
+  |  BOOLEAN
+  |  STRING
+  |  CHARACTER
+  |  LEFT_CURLY (IDENTIFIER COLON expression COMMA)* IDENTIFIER COLON expression RIGHT_CURLY
+  |  IDENTIFIER LEFT_SQUARE expression RIGHT_SQUARE
+  |  LEFT_SQUARE (expression COMMA)* expression RIGHT_SQUARE
+  |  LEFT_SQUARE RIGHT_SQUARE
+  |  expression (ASTERISK|DIVIDE|PERCENT|CARROT) expression
+  |  expression (ONES_COMP|PLUS|DASH) expression
+  |  expression (BIT_AND|BIT_OR|XOR|R_SHIFT|L_SHIFT) expression
+  |  FLIP expression
+  |  expression (GREATER|GREATER_OR_EQUAL|LESS|LESS_OR_EQUAL|EQUAL|NOT_EQUAL) expression
+  |  expression (AND|OR) expression
+  |  FILE LEFT_P expression RIGHT_P
+
+```
+
+## Examples
+```
+bs = 00100111b
+mask = 00000001b
+ans = bs and (flip mask)
+print ans //outputs 100110
+```
+
+## How to build and run the interpreter
+Clone a branch of our Terp repository and open up index.html in Google Chrome.
+
+### Notes:
+Math: +,-,*,/,%,^ (exponent)
+Bits: xor, >>, <<, &, |, flip
+comments: ``, #
+functions: define funcName(p1, p2) ... done
+bit strings: 110101b
+loops: while
+conditionals: <=, >=, ==, and, or, !, !=
+if expression then ... done
+one's complement addition: ++
+Dictionaries: {}
+Files: FILE(url)
+Arrays: []
+print: print
+boolean: true, false
---
+++ lexer.js
@@ -0,0 +1,254 @@
+//expressions
+var IDENTIFIER = 'IDENTIFIER';
+var PLUS = 'PLUS';
+var DIVIDE = 'DIVIDE';
+var DASH = 'DASH';
+var ASTERISK = 'ASTERISK';
+var COMMA = 'COMMA';
+var MOD = 'MOD';
+var OCTOTHORP = 'OCTOTHORP';
+var CARROT = 'CARROT';
+var ASSIGN = 'ASSIGN';
+var EOF = 'EOF';
+var STRING = 'STRING';
+
+//blocks
+var IF = 'IF';
+var THEN = 'THEN';
+var ELSE = 'ELSE';
+var DONE = 'DONE';
+var DEFINE = 'DEFINE';
+var WHILE = 'WHILE';
+var PRINT = 'PRINT';
+
+//literals
+var INTEGER = 'INTEGER';
+var CHAR = 'CHAR';
+var STRING = 'STRING';
+var BOOL = 'BOOL';
+var BITSTRING = 'BITSTRING';
+var FILE = 'FILE';
+
+//bit operators
+var FLIP = 'FLIP';
+var BIT_AND = 'BIT_AND';
+var BIT_OR = 'BIT_OR';
+var XOR = 'XOR';
+var R_SHIFT = 'R_SHIFT';
+var L_SHIFT = 'L_SHIFT';
+var ONES_COMP = 'ONES_COMP';
+
+//brackets
+var LEFT_PARENTHESIS = 'LEFT_PARENTHESIS';
+var RIGHT_PARENTHESIS = 'RIGHT_PARENTHESIS';
+var LEFT_SQUARE = 'LEFT_SQUARE';
+var RIGHT_SQUARE = 'RIGHT_SQUARE';
+var LEFT_CURLY = 'LEFT_CURLY';
+var RIGHT_CURLY = 'RIGHT_CURLY';
+
+//comparators
+var MORE_OR_EQUAL = 'MORE_OR_EQUAL';
+var MORE = 'MORE';
+var LESS_OR_EQUAL = 'LESS_OR_EQUAL';
+var LESS = 'LESS';
+var EQUAL = 'EQUAL';
+var NOT_EQUAL = 'NOT_EQUAL';
+
+//boolean operators
+var AND = 'AND';
+var OR = 'OR';
+var NOT = 'NOT';
+
+
+function lex(source){
+  console.log(source);
+
+  var tokens = [];
+  var tokenSoFar = '';
+  var i = 0;
+
+  function has(regex) {
+    return source.charAt(i).match(regex);
+  }
+
+  function devour() {
+    tokenSoFar += source.charAt(i);
+    i += 1;
+  }
+
+  function skip() {
+    i += 1;
+  }
+
+  function emit(type) {
+    tokens.push({type: type, source: tokenSoFar});
+    tokenSoFar = '';
+  }
+
+  while (i < source.length) {
+    if (has(/\+/)) {
+      devour();
+      if(has(/\+/)){
+        devour();
+        emit(ONES_COMP);
+      } else {
+        emit(PLUS);
+      }
+    } else if (has(/%/)) {
+      devour();
+      emit(MOD);
+    } else if (has(/,/)) {
+      devour();
+      emit(COMMA);
+    // } else if (has(/\'/)) {
+    //   devour();
+    //   devour();
+    //   if(tokenSoFar == /)
+    //   emit(APOSTROPHE);
+    } else if (has(/\"/)) {
+      skip();
+      while(has(/[^"]/)){
+        devour();
+      }
+      skip();
+      emit(STRING);
+    } else if (has(/=/)){
+      devour();
+      if(has(/=/)){
+        devour();
+        emit(EQUAL);
+      } else {
+        emit(ASSIGN);
+      }
+    } else if (has(/</)){
+      devour();
+      if(has(/=/)){
+        devour();
+        emit(LESS_OR_EQUAL);
+      } else if (has(/</)) {
+        devour();
+        emit(L_SHIFT);
+      } else {
+        emit(LESS);
+      }
+    }else if (has(/>/)){
+      devour();
+      if(has(/=/)){
+        devour();
+        emit(MORE_OR_EQUAL);
+      } else if (has(/>/)) {
+        devour();
+        emit(R_SHIFT);
+      } else {
+        emit(MORE);
+      }
+    } else if (has(/!/)) {
+      devour();
+      if(has(/=/)){
+        devour();
+        emit(NOT_EQUAL);
+      } else {
+        emit(NOT);
+      }
+    } else if (has(/&/)) {
+      devour();
+      emit(BIT_AND);
+    } else if(has(/\|/)) {
+      devour();
+      emit(BIT_OR);
+    } else if (has(/-/)) {
+      devour();
+      emit(DASH);
+    } else if (has(/\(/)) {
+      devour();
+      emit(LEFT_PARENTHESIS);
+    } else if (has(/\)/)) {
+      devour();
+      emit(RIGHT_PARENTHESIS);
+    } else if(has(/\[/)) {
+      devour();
+      emit(LEFT_SQUARE);
+    } else if(has(/]/)) {
+      devour();
+      emit(RIGHT_SQUARE);
+    } else if(has(/{/)) {
+      devour();
+      emit(LEFT_CURLY);
+    } else if(has(/\}/)) {
+      devour();
+      emit(RIGHT_CURLY);
+    } else if (has(/\*/)) {
+      devour();
+      emit(ASTERISK);
+    } else if (has(/\^/)) {
+      devour();
+      emit(CARROT);
+    } else if (has(/\//)) {
+      devour();
+      emit(DIVIDE);
+    } else if (has(/\d/)) {
+      while (has(/\d/)) {
+        devour();
+      }
+      if(has(/b/)){
+        skip();
+        if(tokenSoFar.match(/^[01]+$/)){
+          emit(BITSTRING);
+        } else {
+          throw 'bitstring is not in the correct binary format: [' + tokenSoFar + ']';
+        }
+      } else {
+        emit(INTEGER);
+      }
+    } else if (has(/[a-zA-Z]/)) {
+      while (has(/[a-zA-Z0-9]/)) {
+        devour();
+      }
+
+      if(tokenSoFar == 'if') {
+        emit(IF);
+      }else if(tokenSoFar == 'else'){
+        emit(ELSE);
+      } else if(tokenSoFar == 'done'){
+        emit(DONE);
+      } else if(tokenSoFar == 'while'){
+        emit(WHILE);
+      } else if(tokenSoFar == 'print'){
+        emit(PRINT);
+      } else if(tokenSoFar == 'flip'){
+        emit(FLIP);
+      } else if(tokenSoFar == 'and'){
+        emit(AND);
+      } else if(tokenSoFar == 'or') {
+        emit(OR);
+      } else if(tokenSoFar == 'then'){
+        emit(THEN);
+      } else if(tokenSoFar == 'define') {
+        emit(DEFINE);
+      } else if(tokenSoFar == 'xor') {
+        emit(XOR);
+      } else if(tokenSoFar == 'FILE'){
+        emit(FILE);
+      } else if(tokenSoFar == 'true' || tokenSoFar == 'false'){
+          emit(BOOL);
+      } else {
+        emit(IDENTIFIER)
+      }
+    } else if (has(/#/)){
+      devour();
+      while (has(/[^\n]/)){
+        devour();
+      }
+      tokenSoFar = '';
+    } else if (has(/\s/)) {
+      devour();
+      tokenSoFar = '';
+    } else {
+      devour();
+      throw 'unknown token: [' + tokenSoFar + ']';
+    }
+  }
+
+  emit(EOF);
+  console.log(tokens);
+}
